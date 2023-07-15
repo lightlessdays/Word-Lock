a45e87b902ce8a750025767519ccac5c9868241a
Dhruv Badaya <dhruvbadaya@gmail.com>
2023-07-15 22:14:21 +0530

    Update constants.dart

--- lib/fetch/constants.dart
+++ lib/fetch/constants.dart
@@ -1,5 +1,4 @@
-const apiKey1="2eeb25a8-c9fa-4fc8-94ba-fec9e261fb22";
-const apiKey2="a9424fbc-479f-4ce3-b79e-f99b9dbc764e";
+const apiKey1="we are removing the api key from here for privacy reasons";
 
 String fetchURL(String word,String apiKey){
   return "https://www.dictionaryapi.com/api/v3/references/collegiate/json/$word?key=$apiKey";
